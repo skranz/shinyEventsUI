32055bffa4e19416fc9575caee6d9f4977336144
Sebastian Kranz <sebastian.kranz@uni-ulm.de>
2016-06-27 21:35:14 +0200

    Updates

--- R/layout.r
+++ R/layout.r
@@ -1,3 +1,132 @@
+resize.event.example = function() {
+  style = tags$style(HTML('
+  .ui-layout-pane {
+  	background:	#FFF;
+  	border:		none;
+  	padding:	0px;
+  	overflow:	auto;
+  }
+  .ui-layout-pane {
+  	overflow:	hidden;
+  }
+  '))
+  json.opts =
+'
+defaults: {
+  resizable: false,
+  closable: false,
+  slideable: false,
+  spacing_open: 0
+},
+north: {
+  size: "auto",
+},
+'
+
+
+  js = "
+  // select the target node
+  var target = document.querySelector('#myout');
+
+  // create an observer instance
+  var observer = new MutationObserver(function(mutations) {
+      mutations.forEach(function(mutation) {
+          alert('content changed!');
+      });
+  });
+
+  // configuration of the observer:
+  var config = { attributes: true, childList: true, characterData: true }
+
+  // pass in the target node, as well as the observer options
+  observer.observe(target, config);
+
+  "
+
+  app = eventsApp()
+  app$ui = bootstrapPage(
+    jqueryLayoutHeader(),
+    jqueryLayoutPanes(id="layout", style=style, json.opts = json.opts,
+      north = autoResizeLayoutDiv(id = "north", layout.id="layout",
+        h4("Title"),
+        uiOutput("myout")
+      ),
+      center = div(
+        actionButton("btn","Fill"),
+        {
+          txt =paste0("content ", 1:300, collapse=" ")
+          p(txt)
+        }
+      )
+    )
+    #tags$script(HTML(js))
+  )
+  buttonHandler("btn", function(...) {
+    txt =paste0("content ", 1:sample.int(n=100,size=1), collapse=" ")
+    setUI("myout",p(txt))
+    txt =paste0("content ", 1:sample.int(n=100,size=1), collapse=" ")
+    setUI("myout",p(txt))
+  })
+  viewApp(app)
+
+  app = eventsApp()
+  app$ui = bootstrapPage(
+    jqueryLayoutHeader(),
+    frozenHeaderPane(id="layout",auto.resize = TRUE,
+      head = tagList(
+        h4("Title"),
+        uiOutput("myout")
+      ),
+      content = div(
+        actionButton("btn","Fill"),
+        {
+          txt =paste0("content ", 1:300, collapse=" ")
+          p(txt)
+        }
+      )
+    )
+  )
+  buttonHandler("btn", function(...) {
+    txt =paste0("content ", 1:sample.int(n=100,size=1), collapse=" ")
+    setUI("myout",p(txt))
+    txt =paste0("content ", 1:sample.int(n=100,size=1), collapse=" ")
+    setUI("myout",p(txt))
+  })
+  viewApp(app)
+
+}
+
+nested.layout.example = function() {
+
+  app = eventsApp()
+  app$ui = bootstrapPage(
+    jqueryLayoutHeader(),
+    actionButton("btn","Show Layout"),
+    uiOutput("mainUI")
+  )
+  buttonHandler("btn", function(...) {
+    ui = tagList(
+      div(id="mydiv",style="height: 400px",
+        jqueryLayoutPanes(id="mainLayout", parent = "#mydiv",
+          center=div(paste0("Center ",1:5,collapse="\n ")),
+          east=frozenHeaderPane(id="east",parent.layout = "mainLayout",parent.pane="east",
+              head = div("Title Line",actionButton("addBtn","Add"), uiOutput("titleUI")),
+              content = div(paste0("  east center ", 1:100, collapse=" "))
+          )
+        )
+      )
+    )
+    setUI("mainUI",ui)
+  })
+  buttonHandler("addBtn", function(...) {
+    dsetUI("titleUI",p(paste0("more title ", 1:sample.int(n=100,size=1), collapse = " ")))
+    #resizeLayout("east")
+  })
+  viewApp(app)
+
+
+}
+
 dyn.layout.example = function() {
   app = eventsApp()
   style = tags$style(HTML('
@@ -99,6 +228,44 @@ style = tags$style(HTML('
 }
 
 
+frozenHeaderPane = function(id=NULL, header=NULL, content=NULL, parent="body", parent.layout=NULL, parent.pane="center", style = NULL, json.opts=NULL, auto.resize = TRUE) {
+  restore.point("frozenHeaderPane")
+
+  if (is.null(id)) {
+    id = paste0("frozenHeaderPane" , sample.int(size = 1, n=100000000))
+  }
+  style = tags$style(HTML(paste0(
+    style,'
+    #',id,'_north {
+  	  overflow:	hidden;
+    }
+  ')))
+
+  if (is.null(json.opts)) {
+    json.opts ="
+defaults: {
+  resizable: false,
+  closable: false,
+  slideable: false,
+  spacing_open: 0
+},
+north: {
+  size: 'auto'
+}
+  "
+  }
+
+  tagList(
+    jqueryLayoutHeader(),
+    jqueryLayoutPanes(id=id,parent = parent, parent.layout=parent.layout, parent.pane = parent.pane, style=style, json.opts = json.opts,
+      north = autoResizeLayoutDiv(id = paste0(id,"_header"), layout.id=id, auto.resize=auto.resize,
+        header
+      ),
+      center = content
+    )
+  )
+}
+
 #' A jqueryLayoutPage
 #' @export
 jqueryLayoutHeader = function(style=NULL) {
@@ -108,7 +275,15 @@ jqueryLayoutHeader = function(style=NULL) {
     singleton(tags$head(tags$script(src="shared/jqueryui/jquery-ui.min.js"))),
     singleton(tags$head(tags$link(href="shared/jqueryui/jquery-ui.min.css", rel="stylesheet"))),
     singleton(tags$head(tags$link(href="shinyEventsUI/layout-default.css", type="text/css", rel="stylesheet"))),
-    singleton(tags$head(tags$script(src="shinyEventsUI/jquery.layout.js")))
+    singleton(tags$head(tags$script(src="shinyEventsUI/jquery.layout.js"))),
+    singleton(tags$head(tags$style("
+.ui-layout-pane {
+	background:	#FFF;
+	border:		none;
+	padding:	0px;
+	overflow:	auto;
+}
+    ")))
   )
 
 }
@@ -116,35 +291,71 @@ jqueryLayoutHeader = function(style=NULL) {
 
 #' A jqueryLayoutPage
 #' @export
-jqueryLayoutPanes = function(center=NULL, north=NULL,west=NULL, east=NULL, south=NULL, panes=list(center=center, north=north, west=west, east=east, south=south), json.opts="", style=NULL, js.do.layout = TRUE, parent = "body") {
+jqueryLayoutPanes = function(id = NULL, center=NULL, north=NULL,west=NULL, east=NULL, south=NULL, panes=list( north=north, west=west,center=center, east=east, south=south), json.opts="", style=NULL, js.do.layout = TRUE, parent = "body", parent.layout = NULL, parent.pane = "center") {
   restore.point("jqueryLayoutPanes")
+
+  if (is.null(id)) {
+    id = paste0("jqueryLayout" , sample.int(size = 1, n=100000000))
+    if (!is.null(parent.layout)) {
+      id = paste0(parent.layout,"_",parent.pane,"_layout")
+    }
+  }
 
 
   for (dir in names(panes)) {
-    if (!is.null(panes[[dir]]))
-      panes[[dir]] = div(class=paste0("ui-layout-",dir),panes[[dir]])
+    if (!is.null(panes[[dir]])) {
+      class = paste0("ui-layout-",dir)
+      pane.id = paste0(id,"_", dir)
+      panes[[dir]] = div(class=class, id=pane.id,panes[[dir]])
+    }
   }
 
   if (js.do.layout) {
-    var = gsub("#","",parent, fixed=TRUE)
-    js = paste0('
-      var ',var,'__Layout = $("',parent,'").layout({
-        ', json.opts,'
-      });
-    ')
+    if (is.null(parent.layout)) {
+      js = paste0('\nvar ',id,'LayoutVar = $("',parent,'").layout({', json.opts,'});')
+    } else {
+      js = paste0('\nvar ',id,'LayoutVar = ',parent.layout,'LayoutVar.panes.',parent.pane,'.layout({', json.opts,'});')
+    }
   } else {
     js = NULL
   }
   tagList(
+    bottomScript(HTML(js)),
     style,
-    panes,
-    tags$script(HTML(js))
+    panes
   )
+}
+
+autoResizeLayoutDiv = function(id,  layout.id=NULL,..., layout.var = paste0(layout.id, "LayoutVar"), auto.resize = TRUE) {
+  restore.point("autoResizeLayoutDiv")
+  if (!auto.resize) {
+    return(div(id=id,...))
+  }
+
+  js = paste0('
+  // create an observer instance
+  new MutationObserver(function(mutations) {
+    //alert("Resize event!");
+    ', layout.var,'.resizeAll();
+  }).observe(
+    document.querySelector("#',id,'"),
+    {attributes: true,  subtree: true, childList: true, characterData: true }
+  );
+  ')
+
+  tagList(
+    div(id=id, ...),
+    bottomScript(HTML(js))
+  )
+}
+
+autoLayoutResizer = function(id,  layout.id=NULL,..., layout.var = paste0(layout.id, "LayoutVar")) {
+
 }
 
 #' Resize a jqueryLayout. May be useful if inner html has been dynamically changed and we have size="auto"
 #' @export
-resizeLayout = function(layout.var) {
+resizeLayout = function(id, layout.var=paste0(id,"LayoutVar")) {
   js = paste0('
     ',layout.var,'.resizeAll();
   ')
